8b3e411d176251661df8bb0ed30481ecc33d50f2
freja96 <almat1996aaa@gmail.com>
2017-12-07 00:04:05 +0600

    SQL dump

---
+++ neww.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 06, 2017 at 06:03 PM
+-- Server version: 5.7.20-0ubuntu0.17.10.1
+-- PHP Version: 7.1.11-0ubuntu0.17.10.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `neww`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(20) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `headline` varchar(30) NOT NULL,
+  `story` text NOT NULL,
+  `timeadd` timestamp(2) NOT NULL DEFAULT CURRENT_TIMESTAMP(2)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `news`
+--
+
+INSERT INTO `news` (`id`, `name`, `headline`, `story`, `timeadd`) VALUES
+(2, 'qwe', 'qwe', 'Its updated story', '2017-12-06 14:07:08.41');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `login` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `joindate` timestamp(2) NOT NULL DEFAULT CURRENT_TIMESTAMP(2)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `email`, `password`, `joindate`) VALUES
+(1, 'qwe', 'varqwe', 'qwe', '2017-12-05 14:44:50.68'),
+(2, 'Almat', 'almat1996@live.ru', 'qwe', '2017-12-05 15:14:07.43'),
+(3, 'Almat', 'almat1996@live.ru', 'qwe', '2017-12-05 15:14:34.64'),
+(4, 'Almat', 'almat1996@live.ru', '76d80224611fc919a5d54f0ff9fba446', '2017-12-05 15:38:58.11'),
+(5, 'rem', 'rem@mail.ru', '76d80224611fc919a5d54f0ff9fba446', '2017-12-06 17:14:33.29'),
+(6, 'Esdeath', 'esdeath@mail.ru', '76d80224611fc919a5d54f0ff9fba446', '2017-12-06 17:16:30.77'),
+(7, 'Esdeath', 'esdeath@mail.ru', '76d80224611fc919a5d54f0ff9fba446', '2017-12-06 17:17:43.26'),
+(8, 'Kero', 'kero', '76d80224611fc919a5d54f0ff9fba446', '2017-12-06 17:18:46.16'),
+(9, 'Hello', 'hello', '76d80224611fc919a5d54f0ff9fba446', '2017-12-06 17:20:51.03');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
