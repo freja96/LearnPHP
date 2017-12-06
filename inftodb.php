e53c4477ac382b18973f7a1e6e990d6f77bed174
freja96 <almat1996aaa@gmail.com>
2017-12-06 23:01:27 +0600

    New Files

---
+++ inftodb.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($_POST[add]))
+{
+
+$connect = mysqli_connect("localhost", "root","root", "neww");
+if ($connect->connect_error) {
+   die("Connection failed: " . $connect->connect_error);
+} 
+	$query = "INSERT INTO news (name, headline, story)
+	VALUES ('$_POST[name]','$_POST[headline]','$_POST[story]')";
+	if ($connect->query($query) === TRUE) {
+    		 echo "New record created successfully";
+    		 echo "<a href='menu.php'>Here  your news/story</a>";
+			} else {
+    			echo "Error: " . $query . "<br>" . $connect->error;
+			}
+}
+	$queryshow = "SELECT (id, name, headline, story) FROM news ORDER BY dateadd DESC";
+	if ( $connect->queryshow($queryshow)== TRUE)
+	{
+
+	}else{"Error";}
+
+
+?>
